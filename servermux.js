db689bda6714aa4ebe05361d36f283da5351375c
Cesar Zambrano <cesarzambranod@gmail.com>
2020-01-21 12:05:23 -0300

    first commit

---
+++ servermux.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use(express.static(__dirname + '/asset/'));
+//Store all JS and CSS in Scripts folder.
+
+app.use('/', router);
+app.listen(process.env.port || 3500);
+
+console.log('Running at Port 3500');
